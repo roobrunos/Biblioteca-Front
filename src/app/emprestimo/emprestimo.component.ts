46e0fcb8e209ef1d26bf1ce1badf44673d95a2d5
Rodrigo Bruno <rbruno.10@hotmail.com>
2025-05-27 20:36:44 -0300

    frontend biblioteca

---
+++ src/app/emprestimo/emprestimo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emprestimo',
+  imports: [],
+  templateUrl: './emprestimo.component.html',
+  styleUrl: './emprestimo.component.scss'
+})
+export class EmprestimoComponent {
+
+}
