c3acb9bf2715bfcba21a73c6468c35061045b936
Rodrigo Bruno <rbruno.10@hotmail.com>
2025-05-27 21:40:09 -0300

    Atualização1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LivroComponent } from './livro/livro.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { EmprestimoComponent } from './emprestimo/emprestimo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'livro', pathMatch: 'full' },
+  { path: 'livro', component: LivroComponent },
+  { path: 'usuario', component: UsuarioComponent },
+  { path: 'emprestimo', component: EmprestimoComponent }
+];
